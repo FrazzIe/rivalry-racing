822d8e276056e512f06e2434984469015d7dc891
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-04-10 19:37:01 +0100

    Add resource file to solution

---
+++ rivalry-racing/__resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version "05cfa83c-a124-4cfa-a768-c24a5811d8f9"
+
+shared_scripts {
+	"Newtonsoft.Json.dll",
+}
+
+client_scripts {
+	"StreetRacing.Client.net.dll",
+}
+
+server_scripts {
+	"StreetRacing.Server.net.dll",
+}
