9370c595e22b94cc7eacb99e13f941c75d1a7d88
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-04-10 19:44:34 +0100

    Created a base for a race command

--- src/StreetRacing.Server/Server.cs
+++ src/StreetRacing.Server/Server.cs
@@ -19,5 +19,37 @@ namespace StreetRacing.Server
         {
 
         }
+        [Command("race")]
+        private void OnRaceCommand([FromSource] Player player, string[] args)
+        {
+            var messageObject = new
+            {
+                color = new[] { 255, 0, 0 },
+                multiline = true,
+                args = new[] { "Race", "" },
+            };
+            if (args.Length > 0) {
+                switch (args[0])
+                {
+                    case "create":
+                        break;
+                    case "start":
+                        break;
+                    case "join":
+                        break;
+                    case "cancel":
+                        break;
+                    case "leave":
+                        break;
+                    default:
+                        messageObject.args[1] = "Invalid syntax: ^*^1create^r^0, ^*^1start, ^*^1join, ^*^1cancel ^r^0or ^*^1leave ^r^0are the only accepted arguments!!";
+                        player.TriggerEvent("chat:addMessage", messageObject);
+                        break;
+            } else
+            {
+                messageObject.args[1] = "Invalid syntax: ^*^1create^r^0, ^*^1start, ^*^1join, ^*^1cancel ^r^0or ^*^1leave ^r^0are the only accepted arguments!!";
+                player.TriggerEvent("chat:addMessage", messageObject);
+            }
+        }
     }
 }
