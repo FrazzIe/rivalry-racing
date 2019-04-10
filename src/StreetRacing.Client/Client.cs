bd1707c49787d93f730536d49064ad99c999e62a
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-04-10 19:36:07 +0100

    Upload solution files

---
+++ src/StreetRacing.Client/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using Newtonsoft.Json;
+
+namespace StreetRacing.Client
+{
+    public class Client : BaseScript
+    {
+        public Client()
+        {
+
+        }
+    }
+}
