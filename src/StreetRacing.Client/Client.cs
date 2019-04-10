ce3795e2d7d130872336dd10e7d4db40f6b87dc3
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2019-04-10 21:22:23 +0100

    endpoint picker, destination blip, finish line trigger
    - Added a way for the host to pick the endpoint
    - Added a blip for the end point which will display a route on the gps
    - Added a distance check that triggers when close to the finish line

--- src/StreetRacing.Client/Client.cs
+++ src/StreetRacing.Client/Client.cs
@@ -13,6 +13,7 @@ namespace StreetRacing.Client
     public class Client : BaseScript
     {
         Race race;
+        Blip destination;
 
         public Client()
         {
@@ -23,6 +24,61 @@ namespace StreetRacing.Client
         {
             if (race != null)
             {
+                Vector3 position = Game.PlayerPed.Position;
+                string serverId = Game.Player.ServerId.ToString();
+
+                if (!race.Ready())
+                {
+                    if (race.ID == serverId)
+                    {
+                        if (race.StartPoint == Vector3.Zero) { race.StartPoint = position; World.RemoveWaypoint(); Debug.WriteLine("Setup start point!"); }
+                        else
+                        {
+                            Blip waypointBlip = World.GetWaypointBlip();
+
+                            API.ClearPrints();
+                            API.BeginTextCommandPrint("STRING");                            
+                            API.AddTextComponentSubstringPlayerName("Mark the finish line on your gps!");
+                            API.EndTextCommandPrint(0, true);
+
+                            if (waypointBlip != null)
+                            {
+                                race.EndPoint = waypointBlip.Position;
+                                race.EndPoint = new Vector3(race.EndPoint.X, race.EndPoint.Y, World.GetGroundHeight(new Vector2(race.EndPoint.X, race.EndPoint.Y)));
+
+                                World.RemoveWaypoint();
+
+                                TriggerServerEvent("Race.Setup", race.StartPoint, race.EndPoint);
+
+                                Debug.WriteLine("Setup endpoint!");
+                            }
+                        }
+                    }
+                } else
+                {
+                    if (race.Started && !race.Finished)
+                    {
+                        if (destination == null)
+                        {
+                            destination = World.CreateBlip(race.EndPoint);
+                            destination.Sprite = BlipSprite.Standard;
+                            destination.Scale = 0.8f;
+                            destination.Color = BlipColor.Yellow;
+                            destination.ShowRoute = true;
+                            World.WaypointPosition = race.EndPoint;
+                        }
+
+                        if (position.DistanceToSquared(race.EndPoint) < 25)
+                        {
+                            TriggerServerEvent("Race.End", race.ID);
+                            race.Finished = true;
+                            destination.Delete();
+                            destination = null;
+                        }
+                    }
+                }
+            }
+        }
             }
         [EventHandler("Race.Sync")]
         private void SyncRace(string _race)
